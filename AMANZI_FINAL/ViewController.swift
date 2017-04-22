df1d309a5e751bd732e4368ddef298f64c2d49b4
Mary Nugent <gn20895@aol.com>
2017-04-21 22:42:18 -0400

    Initial Commit

---
+++ AMANZI_FINAL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AMANZI_FINAL
+//
+//  Created by Brennan Nugent on 4/21/17.
+//  Copyright © 2017 Brennan Nugent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
